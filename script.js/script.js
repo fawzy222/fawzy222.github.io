cccf9b6c4838c96fdff23052903dc7b1b9e57dc1
fawzy222 <115642993+fawzy222@users.noreply.github.com>
2023-04-27 05:44:21 -0700

    added email,styles,and responsiveness

--- script.js/script.js
+++ script.js/script.js
@@ -20,6 +20,41 @@ class CssPropControl {
     bodyCssProps.set('--link', bodyCssProps.get(`--${mode}-link`))
   })
 
+  const form = document.getElementById('fs-frm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   
+  const name = document.getElementById('full-name').value;
+  const email = document.getElementById('email-address').value;
+  const message = document.getElementById('message').value;
+  
+  const data = {
+    name,
+    email,
+    message
+  };
+  
+  fetch('https://formspree.io/f/xbjeqvra', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (response.ok) {
+      alert('Thank you for contacting me! I will get back to you soon.');
+      form.reset();
+    } else {
+      alert('Something went wrong. Please try again later.');
+    }
+  })
+  .catch(error => {
+    alert('Something went wrong. Please try again later.');
+    console.error(error);
+  });
+});
+
 
   
